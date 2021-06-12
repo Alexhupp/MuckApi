c18b1834ceec96998f6fb370c0a15bceaa4abcad
Alexhupp <adennishupp@gmail.com>
2021-06-12 14:02:51 -0400

    Update ChatCommands.cs

--- API/ChatCommands.cs
+++ API/ChatCommands.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +33,18 @@ namespace AlexMuckApi.API
             int seed = GameManager.gameSettings.Seed;
             Chat.AppendMessage(-1, string.Concat(new object[] { "<color=", text, ">Seed: ", seed, " (copied to clipboard)<color=white>" }), "");
             UnityEngine.GUIUtility.systemCopyBuffer = string.Concat(seed);
+            return true;
+        }
+        public static bool help(string message)
+        {
+            ChatBox Chat = ChatBox.Instance;
+            Debug.Log("[Command List]");
+            IOrderedEnumerable<KeyValuePair<string, string>> sortedDict =
+                Main.CommandInfo.OrderBy(i => i.Key.Length).ThenBy(i => i.Key);
+            foreach (var item in sortedDict)
+            {
+                Debug.Log("[ " + char.ToUpper(item.Key[0]).ToString() + item.Key.Substring(1) + " ]: " + item.Value);
+            }
             return true;
         }
     }
